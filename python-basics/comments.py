9bc320089ffe42a1357cf5a1f09eef54d17397ca
sandibekova <s_sandibekova@kbtu.kz>
2026-01-30 13:20:02 +0500

    Python scripts for Practice1

---
+++ python-basics/comments.py
@@ -0,0 +1,14 @@
+#print("Hello, World!")
+print("Cheers, Saida!")
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, Saida")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, Suirik!")
---
+++ python-basics/variables.py
@@ -0,0 +1,14 @@
+x = 8
+y = "Saida"
+print(x)
+print(y)
+
+x = 8      
+x = "Saida" 
+print(x)
+
+x = 8
+y = "Saida"
+print(type(x))
+print(type(y))
+
